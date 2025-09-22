29100f478ea553e76e5290ca7484db9d0452925e
Matyáš Jaworek <jaworekm24@spstrutnov.cz>
2025-09-22 14:30:46 +0200

    Hotové kámen nůžky papír

--- Program.cs
+++ Program.cs
@@ -1,10 +1,50 @@
 ﻿namespace prvniprojekty2
 {
-    internal class Program
+    internal class programprvni
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            List<string> seznam = new List<string> { "kámen", "nůžky", "papír" };
+
+            Console.Write("Ahoj, zadej kámen, nůžky, nebo papír: ");
+
+            string vystup;
+            vystup = Console.ReadLine();
+
+            Random vyber = new Random();
+
+            int volba = vyber.Next(seznam.Count);
+            string nahodnaVolba = seznam[volba];
+
+            Console.WriteLine("ja vybral " + nahodnaVolba);
+
+            if (vystup == nahodnaVolba)
+            {
+                Console.WriteLine("Remiza");
+            }
+            else if (vystup == "kámen" && nahodnaVolba == "nůžky")
+            {
+                Console.WriteLine("Vyhrál si");
+            }
+            else if (vystup == "nůžky" && nahodnaVolba == "kámen")
+            {
+                Console.WriteLine("Prohrál si");
+            }
+            else if (vystup == "papír" && nahodnaVolba == "nůžky")
+            {
+                Console.WriteLine("Prohrál si");
+            }
+            else if (vystup == "nůžky" && nahodnaVolba == "papír")
+            {
+                Console.WriteLine("Vyhrál si");
+            }
+
+
+
+
+
+
+
+            }
     }
 }
